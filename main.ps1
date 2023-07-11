a7de578ae18940753b5deb5ff5861d9bd333334f
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-07-11 16:43:17 +0800

    20230711A

--- main.ps1
+++ main.ps1
@@ -1,6 +1,9 @@
 #Requires -PSEdition Core -Version 7.2
 $Script:ErrorActionPreference = 'Stop'
 [Boolean]$IsDebugMode = $Env:RUNNER_DEBUG -ieq '1' -or $Env:RUNNER_DEBUG -ieq 'True'
+[SemVer]$PowerShellGetVersionMaximum = [SemVer]::Parse('2.99999999.99999999')
+[SemVer]$PowerShellGetVersionMinimum = [SemVer]::Parse('2.2.5')
+Write-Host -Object 'Import input.'
 Try {
 	[Boolean]$InputToolkitSetup = [Boolean]::Parse($Env:INPUT_TOOLKIT_SETUP)
 }
@@ -9,10 +12,12 @@ Catch {
 	Exit 1
 }
 Try {
-	[SemVer]$InputToolkitVersion = [SemVer]::Parse($Env:INPUT_TOOLKIT_VERSION)
+	[String]$InputToolkitVersionRaw = $Env:INPUT_TOOLKIT_VERSION
+	[Boolean]$InputToolkitVersionLatest = $InputToolkitVersionRaw -ieq 'Latest'
+	[SemVer]$InputToolkitVersion = $InputToolkitVersionLatest ? [SemVer]::Parse('1.0.0-fake') : [SemVer]::Parse($InputToolkitVersionRaw)
 }
 Catch {
-	Write-Host -Object '::error::Input `toolkit_version` must be type of SemVer!'
+	Write-Host -Object '::error::Input `toolkit_version` must be type of SemVer, or `"Latest"`!'
 	Exit 1
 }
 Try {
@@ -22,17 +27,18 @@ Catch {
 	Write-Host -Object '::error::Input `toolkit_allowprerelease` must be type of boolean!'
 	Exit 1
 }
+Write-Host -Object 'Setup PowerShell Gallery.'
 Try {
-	$PSRepositoryPSGallery = Get-PSRepository -Name 'PSGallery'
+	$PSRepositoryPSGalleryMeta = Get-PSRepository -Name 'PSGallery'
 }
 Catch {
 	Write-Host -Object '::error::PowerShell repository `PSGallery` does not exist!'
 	Exit 1
 }
-If ($PSRepositoryPSGallery.InstallationPolicy -ine 'Trusted') {
+If ($PSRepositoryPSGalleryMeta.InstallationPolicy -ine 'Trusted') {
 	Set-PSRepository -Name 'PSGallery' -InstallationPolicy 'Trusted' -Verbose:$IsDebugMode
 }
-[SemVer]$PowerShellGetMinimumVersion = [SemVer]::Parse('2.2.5')
+Write-Host -Object 'Setup PowerShellGet.'
 Try {
 	$PSModulePowerShellGetMeta = (
 		Get-Module -Name 'PowerShellGet' -ListAvailable -ErrorAction 'SilentlyContinue' |
@@ -42,22 +48,31 @@ Try {
 	) ?? (Get-PackageProvider -Name 'PowerShellGet' -ErrorAction 'SilentlyContinue') ?? (Get-InstalledModule -Name 'PowerShellGet' -ErrorAction 'SilentlyContinue')
 	If (
 		$Null -ieq $PSModulePowerShellGetMeta -or
-		$PSModulePowerShellGetMeta.Version -ilt $PowerShellGetMinimumVersion
+		$PSModulePowerShellGetMeta.Version -ilt $PowerShellGetVersionMinimum
 	) {
 		Throw
 	}
 }
 Catch {
-	Install-Module -Name 'PowerShellGet' -MinimumVersion $PowerShellGetMinimumVersion -Scope 'AllUsers' -AcceptLicense -Confirm:$False -Verbose:$IsDebugMode
+	Install-Module -Name 'PowerShellGet' -MinimumVersion $PowerShellGetVersionMinimum -MaximumVersion $PowerShellGetVersionMaximum -Scope 'AllUsers' -AcceptLicense -Confirm:$False -Verbose:$IsDebugMode
 }
 If ($InputToolkitSetup) {
-	Try {
-		$PSModuleToolkitMeta = Get-InstalledModule -Name 'hugoalh.GitHubActionsToolkit'
-		If ($PSModuleToolkitMeta.Version -ine $InputToolkitVersion) {
-			Throw
-		}
+	Write-Host -Object 'Setup PowerShell module `hugoalh.GitHubActionsToolkit`.'
+	If ($InputToolkitVersionLatest) {
+		Install-Module -Name 'hugoalh.GitHubActionsToolkit' -Scope 'AllUsers' -AllowPrerelease:$InputToolkitAllowPreRelease -AcceptLicense -Confirm:$False -Verbose:$IsDebugMode -PassThru |
+			Format-List |
+			Out-String -Width 120 |
+			Write-Host
 	}
-	Catch {
-		Install-Module -Name 'hugoalh.GitHubActionsToolkit' -RequiredVersion $InputToolkitVersion -Scope 'AllUsers' -AllowPrerelease:$InputToolkitAllowPreRelease -AcceptLicense -Confirm:$False -Verbose:$IsDebugMode
+	Else {
+		Try {
+			$PSModuleToolkitMeta = Get-InstalledModule -Name 'hugoalh.GitHubActionsToolkit'
+			If ($PSModuleToolkitMeta.Version -ine $InputToolkitVersion) {
+				Throw
+			}
+		}
+		Catch {
+			Install-Module -Name 'hugoalh.GitHubActionsToolkit' -RequiredVersion $InputToolkitVersion -Scope 'AllUsers' -AllowPrerelease:$InputToolkitAllowPreRelease -AcceptLicense -Confirm:$False -Verbose:$IsDebugMode
+		}
 	}
 }
