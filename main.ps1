200a3dde0462c4aa003e5c00e3d9cb46366c88ab
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-07-12 17:49:23 +0800

    20230712B

--- main.ps1
+++ main.ps1
@@ -2,9 +2,83 @@
 $Script:ErrorActionPreference = 'Stop'
 Write-Host -Object 'Initialize.'
 [Boolean]$IsDebugMode = $Env:RUNNER_DEBUG -ieq '1' -or $Env:RUNNER_DEBUG -ieq 'True'
+[RegEx]$SemVerRangeRegEx = '^(?:<|<=|=|>=|>|\^|~) *'
+<#
 [SemVer]$PowerShellGetVersionMaximum = [SemVer]::Parse('2.99999999.99999999')
 [SemVer]$PowerShellGetVersionMinimum = [SemVer]::Parse('2.2.5')
+#>
+Function Install-ModuleTargetVersion {
+	[CmdletBinding()]
+	[OutputType([Void])]
+	Param (
+		[Parameter(Mandatory = $True, Position = 0)][Alias('ModuleName')][String]$Name,
+		[Parameter(Mandatory = $True, Position = 1)][AllowEmptyString()][Alias('ModuleVersionModifier')][String]$VersionModifier,
+		[Parameter(Mandatory = $True, Position = 2)][Alias('ModuleVersionNumber')][SemVer]$VersionNumber,
+		[Switch]$AllowPrerelease
+	)
+	[SemVer]$VersionTarget = Find-Module -Name $Name -AllVersions -AllowPrerelease:($AllowPrerelease.IsPresent) -Verbose:$IsDebugMode |
+		Select-Object -ExpandProperty 'Version' |
+		ForEach-Object -Process { [SemVer]::Parse($_) } |
+		Where-Object -FilterScript {
+			If ($VersionModifier -ieq '<') {
+				$_ -ilt $VersionNumber |
+					Write-Output
+			}
+			ElseIf ($VersionModifier -ieq '<=') {
+				$_ -ile $VersionNumber |
+					Write-Output
+			}
+			ElseIf ($VersionModifier -ieq '>') {
+				$_ -igt $VersionNumber |
+					Write-Output
+			}
+			ElseIf ($VersionModifier -ieq '>=') {
+				$_ -ige $VersionNumber |
+					Write-Output
+			}
+			ElseIf ($VersionModifier -ieq '^') {
+				$_ -ige $VersionNumber -and $_ -ilt [SemVer]::Parse("$($VersionNumber.Major + 1).0.0") |
+					Write-Output
+			}
+			ElseIf ($VersionModifier -ieq '~') {
+				$_ -ige $VersionNumber -and $_ -ilt [SemVer]::Parse("$($VersionNumber.Major).$($VersionNumber.Minor + 1).0") |
+					Write-Output
+			}
+			Else {
+				$VersionNumber -ieq $_ |
+					Write-Output
+			}
+		} |
+		Sort-Object |
+		Select-Object -Last 1
+	Try {
+		$ModuleMeta = Get-InstalledModule -Name $Name
+		If ($ModuleMeta.Version -ine $VersionTarget) {
+			Throw
+		}
+	}
+	Catch {
+		Install-Module -Name $Name -RequiredVersion $VersionTarget -Scope 'AllUsers' -AllowPrerelease:($AllowPrerelease.IsPresent) -AcceptLicense -Confirm:$False -Verbose:$IsDebugMode
+	}
+}
 Write-Host -Object 'Import input.'
+Try {
+	[Boolean]$InputPowerShellGetSetup = [Boolean]::Parse($Env:INPUT_POWERSHELLGET_SETUP)
+}
+Catch {
+	Write-Host -Object '::error::Input `powershellget_setup` must be type of boolean!'
+	Exit 1
+}
+Try {
+	[String]$InputPowerShellGetVersionRaw = $Env:INPUT_POWERSHELLGET_VERSION
+	[Boolean]$InputPowerShellGetVersionLatest = $InputPowerShellGetVersionRaw -ieq 'Latest'
+	[String]$InputPowerShellGetVersionModifier = ($InputPowerShellGetVersionRaw -imatch $SemVerRangeRegEx) ? $Matches[0].Trim() : ''
+	[SemVer]$InputPowerShellGetVersionNumber = [SemVer]::Parse((($InputPowerShellGetVersionRaw -imatch $SemVerRangeRegEx) ? $InputPowerShellGetVersionRaw.Replace($Matches[0]) : $InputPowerShellGetVersionRaw))
+}
+Catch {
+	Write-Host -Object '::error::Input `powershellget_version` must be `"Latest"` or type of SemVer!'
+	Exit 1
+}
 Try {
 	[Boolean]$InputToolkitSetup = [Boolean]::Parse($Env:INPUT_TOOLKIT_SETUP)
 }
@@ -15,7 +89,8 @@ Catch {
 Try {
 	[String]$InputToolkitVersionRaw = $Env:INPUT_TOOLKIT_VERSION
 	[Boolean]$InputToolkitVersionLatest = $InputToolkitVersionRaw -ieq 'Latest'
-	[SemVer]$InputToolkitVersion = $InputToolkitVersionLatest ? [SemVer]::Parse('1.0.0-fake') : [SemVer]::Parse($InputToolkitVersionRaw)
+	[String]$InputToolkitVersionModifier = ($InputToolkitVersionRaw -imatch $SemVerRangeRegEx) ? $Matches[0].Trim() : ''
+	[SemVer]$InputToolkitVersionNumber = [SemVer]::Parse((($InputToolkitVersionRaw -imatch $SemVerRangeRegEx) ? $InputToolkitVersionRaw.Replace($Matches[0]) : $InputToolkitVersionRaw))
 }
 Catch {
 	Write-Host -Object '::error::Input `toolkit_version` must be `"Latest"` or type of SemVer!'
@@ -39,6 +114,7 @@ Catch {
 If ($PSRepositoryPSGalleryMeta.InstallationPolicy -ine 'Trusted') {
 	Set-PSRepository -Name 'PSGallery' -InstallationPolicy 'Trusted' -Verbose:$IsDebugMode
 }
+<#
 Write-Host -Object 'Setup PowerShellGet.'
 Try {
 	$PSModulePowerShellGetMeta = (
@@ -57,16 +133,27 @@ Try {
 Catch {
 	Install-Module -Name 'PowerShellGet' -MinimumVersion $PowerShellGetVersionMinimum -MaximumVersion $PowerShellGetVersionMaximum -Scope 'AllUsers' -AcceptLicense -Confirm:$False -Verbose:$IsDebugMode
 }
+#>
+If ($InputPowerShellGetSetup) {
+	Write-Host -Object 'Setup PowerShellGet.'
+	If ($InputPowerShellGetVersionLatest) {
+		Install-Module -Name 'PowerShellGet' -Scope 'AllUsers' -AcceptLicense -Confirm:$False -Verbose:$IsDebugMode
+	}
+	Else {
+		Install-ModuleTargetVersion -Name 'PowerShellGet' -VersionModifier $InputPowerShellGetVersionModifier -VersionNumber $InputPowerShellGetVersionNumber
+	}
+	Get-InstalledModule -Name 'PowerShellGet' -AllVersions |
+		Format-List |
+		Out-String -Width 120 |
+		Write-Host
+}
 If ($InputToolkitSetup) {
 	Write-Host -Object 'Setup PowerShell module `hugoalh.GitHubActionsToolkit`.'
 	If ($InputToolkitVersionLatest) {
 		Install-Module -Name 'hugoalh.GitHubActionsToolkit' -Scope 'AllUsers' -AllowPrerelease:$InputToolkitAllowPreRelease -AcceptLicense -Confirm:$False -Verbose:$IsDebugMode
-		Get-InstalledModule -Name 'hugoalh.GitHubActionsToolkit' -AllVersions -AllowPrerelease |
-			Format-List |
-			Out-String -Width 120 |
-			Write-Host
 	}
 	Else {
+		<#
 		Try {
 			$PSModuleToolkitMeta = Get-InstalledModule -Name 'hugoalh.GitHubActionsToolkit'
 			If ($PSModuleToolkitMeta.Version -ine $InputToolkitVersion) {
@@ -76,5 +163,11 @@ If ($InputToolkitSetup) {
 		Catch {
 			Install-Module -Name 'hugoalh.GitHubActionsToolkit' -RequiredVersion $InputToolkitVersion -Scope 'AllUsers' -AllowPrerelease:$InputToolkitAllowPreRelease -AcceptLicense -Confirm:$False -Verbose:$IsDebugMode
 		}
+		#>
+		Install-ModuleTargetVersion -Name 'hugoalh.GitHubActionsToolkit' -VersionModifier $InputToolkitVersionModifier -VersionNumber $InputToolkitVersionNumber -AllowPrerelease:$InputToolkitAllowPreRelease
 	}
+	Get-InstalledModule -Name 'hugoalh.GitHubActionsToolkit' -AllVersions -AllowPrerelease |
+		Format-List |
+		Out-String -Width 120 |
+		Write-Host
 }
